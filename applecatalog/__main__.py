f7aed744eaec1b44c9c7c26f06512c85481ca661
doron zarhi <doron88@gmail.com>
2023-02-23 16:01:29 +0200

    refactor package

--- applecatalog/__main__.py
+++ applecatalog/__main__.py
@@ -1,52 +1,35 @@
 import logging
 import os
-import shutil
-import tempfile
+from pathlib import Path
 from pprint import pprint
 
 import click
 import coloredlogs
 
-from applecatalog.catalog import Catalog
+from applecatalog.catalog import Catalog, ProductInfo
 
 coloredlogs.install(level=logging.DEBUG)
 
 logging.getLogger('urllib3.connectionpool').disabled = True
 
 
-def extract_package(filename, out_dir):
+def extract_package(filename: Path, out_dir: Path) -> None:
     assert 0 == os.system(f'pkgutil --expand "{filename}" "{out_dir}"')
     payload = os.path.join(out_dir, 'Payload')
     assert 0 == os.system(f'tar xf "{payload}" -C "{out_dir}"')
 
 
-def get_unique_product(catalog: Catalog, match):
+def get_unique_product(match: str) -> ProductInfo:
     products = []
-    for product in catalog.products(detailed=False):
+    for product in Catalog().products(detailed=False):
         if product.basename and match in product.basename:
             products.append(product)
     assert len(products) == 1
     return products[0]
 
 
-def get_xprotect_product(catalog: Catalog):
-    return get_unique_product(catalog, 'XProtect')
-
-
-class Command(click.Command):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.params[:0] = [
-            click.Option(('catalog', '--type'), type=click.Choice(['seed', 'beta', 'costumerseed']), default='seed',
-                         callback=self.catalog),
-        ]
-
-    @staticmethod
-    def catalog(ctx, param, value):
-        if '_APPCAT_COMPLETE' in os.environ:
-            # prevent connection establishment when in autocomplete mode
-            return
-        return Catalog(value)
+def get_xprotect_product() -> ProductInfo:
+    return get_unique_product('XProtectPayloads')
 
 
 @click.group()
@@ -54,10 +37,10 @@ def cli():
     pass
 
 
-@cli.command(cls=Command)
-def date(catalog):
+@cli.command()
+def date():
     """ last update date """
-    print(catalog.date)
+    print(Catalog().date)
 
 
 @cli.group()
@@ -66,29 +49,29 @@ def products():
     pass
 
 
-@products.command('list', cls=Command)
+@products.command('list')
 @click.option('-q', '--quick', is_flag=True, help='don\'t require extended information')
-def products_list(catalog, quick):
+def products_list(quick):
     """ list all products """
-    for product in catalog.products(detailed=not quick):
+    for product in Catalog().products(detailed=not quick):
         print(product)
 
 
-@products.command('download', cls=Command)
+@products.command('download')
 @click.argument('product_id')
 @click.argument('out_dir', type=click.Path(dir_okay=True, exists=False))
-def products_download(catalog, product_id, out_dir):
+def products_download(product_id: str, out_dir: str):
     """ download a single product packages """
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
-    catalog.download(product_id, out_dir)
+    Catalog().download(product_id, out_dir)
 
 
-@products.command('info', cls=Command)
+@products.command('info')
 @click.argument('product_id')
-def products_info(catalog, product_id):
+def products_info(product_id: str):
     """ query info for a single product """
-    pprint(catalog.get_product(product_id))
+    pprint(Catalog().get_product(product_id))
 
 
 @products.group()
@@ -97,10 +80,10 @@ def macos():
     pass
 
 
-@macos.command('list', cls=Command)
-def macos_list(catalog):
+@macos.command('list')
+def macos_list():
     """ list all macos products """
-    for k in catalog.macos_products:
+    for k in Catalog().macos_products:
         print(k)
 
 
@@ -110,40 +93,11 @@ def xprotect():
     pass
 
 
-@xprotect.command('date', cls=Command)
-def xprotect_date(catalog):
+@xprotect.command('date')
+def xprotect_date():
     """ XProtect update date """
-    product = get_xprotect_product(catalog)
+    product = get_xprotect_product()
     print(product.date)
-
-
-@xprotect.command('download', cls=Command)
-@click.argument('out_path', type=click.Path(dir_okay=True, file_okay=False, exists=False))
-def xprotect_download(catalog, out_path):
-    """ download latest xprotect rules """
-    if not os.path.exists(out_path):
-        os.makedirs(out_path)
-
-    product = get_xprotect_product(catalog)
-
-    with tempfile.TemporaryDirectory() as download_temp_dir:
-        # download XProtect package
-        results = catalog.download(product.id, download_temp_dir)
-
-        # verify exactly one was found
-        assert len(results) == 1, 'expected exactly one result'
-
-        # extract the XProtect package
-        package_temp_dir = os.path.join(download_temp_dir, 'package')
-        extract_package(results[0], package_temp_dir)
-        resource_dir = os.path.join(package_temp_dir,
-                                    'Library/Apple/System/Library/CoreServices/XProtect.bundle/Contents/Resources')
-
-        for filename in os.listdir(resource_dir):
-            # copy to target directory
-            logging.debug(f'copying {filename} from extracted package...')
-            filename = os.path.join(resource_dir, filename)
-            shutil.copyfile(filename, os.path.join(out_path, os.path.basename(filename)))
 
 
 if __name__ == '__main__':
--- applecatalog/catalog.py
+++ applecatalog/catalog.py
@@ -1,22 +1,25 @@
+import datetime
 import gzip
 import logging
-import os
 import plistlib
 from collections import namedtuple
+from pathlib import Path
+from typing import Generator, List, Mapping
 
 import requests
+from parameter_decorators import str_to_path
 from tqdm import tqdm
 
 APPLE_SEED_URL = 'https://swscan.apple.com/content/catalogs/others/index-13-12-10.16-10.15-10.14-10.13-10.12-10.11-' \
                  '10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz'
-MacOsProduct = namedtuple('MacOsProduct', 'product name build version')
-Product = namedtuple('Product', 'id version title date basename')
+MacOsProductInfo = namedtuple('MacOsProduct', 'product name build version')
+ProductInfo = namedtuple('ProductInfo', 'id version title date basename')
 
 
-def download_file(url, out_dir):
+def download_file(url: str, out_dir: Path) -> Path:
     local_filename = url.split('/')[-1]
     logging.debug(f'downloading: {local_filename}')
-    local_filename = os.path.join(out_dir, local_filename)
+    local_filename = out_dir / local_filename
     with requests.get(url, stream=True) as r:
         r.raise_for_status()
         total_size_in_bytes = int(r.headers.get('content-length', 0))
@@ -32,23 +35,21 @@ def download_file(url, out_dir):
 
 
 class Catalog:
-    def __init__(self, type_='seed'):
-        self._type = type_
+    def __init__(self):
         self._catalog = {}
         self.reload()
 
-    def reload(self):
-        self._catalog = plistlib.loads(
-            gzip.decompress(requests.get(APPLE_SEED_URL.format(type=self._type), verify=False).content))
+    def reload(self) -> None:
+        self._catalog = plistlib.loads(gzip.decompress(requests.get(APPLE_SEED_URL, verify=False).content))
 
     @property
-    def date(self):
+    def date(self) -> datetime.datetime:
         return self._catalog.get('IndexDate', None)
 
-    def get_product(self, product_id: str):
+    def get_product(self, product_id: str) -> Mapping:
         return self._catalog['Products'].get(product_id)
 
-    def get_product_info(self, product_id: str, detailed=True):
+    def get_product_info(self, product_id: str, detailed=True) -> ProductInfo:
         product = self.get_product(product_id)
         date = product.get('PostDate')
         title = None
@@ -64,14 +65,14 @@ class Catalog:
                 if english is None:
                     english = localization.get('en')
                 title = english['title']
-        return Product(id=product_id, version=version, title=title, date=date, basename=basename)
+        return ProductInfo(id=product_id, version=version, title=title, date=date, basename=basename)
 
-    def products(self, detailed=True):
+    def products(self, detailed=True) -> Generator[ProductInfo, None, None]:
         for product_id, product in self._catalog['Products'].items():
             yield self.get_product_info(product_id, detailed=detailed)
 
     @property
-    def macos_products(self):
+    def macos_products(self) -> Generator[MacOsProductInfo, None, None]:
         for product_id, product in self._catalog['Products'].items():
             extended_meta_info = product.get('ExtendedMetaInfo')
             if extended_meta_info is None:
@@ -92,10 +93,11 @@ class Catalog:
             auxinfo = metadata.split('<auxinfo>')[1].split('</auxinfo>')[0].encode()
             auxinfo = plistlib.loads(b'<plist version="1.0">' + auxinfo + b'</plist>')
 
-            yield MacOsProduct(product=product_id, name=name, build=auxinfo.get('BUILD'),
-                               version=auxinfo.get('VERSION'))
+            yield MacOsProductInfo(product=product_id, name=name, build=auxinfo.get('BUILD'),
+                                   version=auxinfo.get('VERSION'))
 
-    def download(self, product_id: str, out_dir: str):
+    @str_to_path('out_dir')
+    def download(self, product_id: str, out_dir: Path) -> List[Path]:
         results = []
         product = self._catalog['Products'][product_id]
         for package in product['Packages']:
